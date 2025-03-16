cbee950d68566e100e6d29c7380dd0506c161937
scott <sktatsuno@gmail.com>
2025-03-15 21:53:41 -0700

    Fix line length warning

--- 1_Arrays_Hashing/Medium/0112_Best_Time_to_Buy_and_Sell_Stock_II/solution.py
+++ 1_Arrays_Hashing/Medium/0112_Best_Time_to_Buy_and_Sell_Stock_II/solution.py
@@ -5,8 +5,10 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # Track profit
         profit = 0
-        # If price of i + 1 day is greater than i then buy i and sell at i + 1 realizing profit immediately
-        # Number of transactions doesn't matter so can take advantage of that for simpler code
+        # If price of i + 1 day is greater than i then buy i and sell at i + 1
+        # realizing profit immediately
+        # Number of transactions doesn't matter so can take advantage of that
+        # for simpler code
         # end iteration 1 short of end since have essentially 2 pointers
         for i in range(len(prices) - 1):
             if prices[i + 1] > prices[i]:
