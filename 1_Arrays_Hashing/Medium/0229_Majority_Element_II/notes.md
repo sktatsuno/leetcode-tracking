27a5a616603f267c3c8b7828ed909f14fd2997ef
scott <sktatsuno@gmail.com>
2025-03-17 19:08:26 -0700

    failed first attempt

---
+++ 1_Arrays_Hashing/Medium/0229_Majority_Element_II/notes.md
@@ -0,0 +1,6 @@
+**3/17/25** 
+- Only a max of 2 elements could appear more than 1/3 of the time
+- In majority element I incremented or decremented a number to represent votes towards one number or the other
+- can't necessarily do that with 1/3 right
+- Using 2 different counter and 2 different candidates
+- I got close to getting the first pass correct. But needed help to do second pass
---
+++ 1_Arrays_Hashing/Medium/0229_Majority_Element_II/solution.py
@@ -0,0 +1,47 @@
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        # Only a max of 2 elements could show up
+        # How can we solve in linear time and O(1) space?
+        # Need to use voting algorithm, but it is tracking more than 2 options
+        if not nums:
+            return []
+
+        counter_1, counter_2 = 0, 0
+        candidate_1, candidate_2 = None, None
+        result = []
+        threshold = len(nums) // 3
+
+        # First pass: find potential candidates
+        for num in nums:
+            if num == candidate_1:
+                counter_1 += 1
+            elif num == candidate_2:
+                counter_2 += 1
+            # if one of candidates is surpassed replace
+            # and reset counter
+            elif counter_1 == 0:
+                candidate_1, counter_1 = num, 1
+            elif counter_2 == 0:
+                candidate_2, counter_2 = num, 1
+            else:
+                counter_1 -= 1
+                counter_2 -= 1
+
+        # Second Pass: Validate candidates
+        counter_1, counter_2 = 0, 0
+        for num in nums:
+            if num == candidate_1:
+                counter_1 += 1
+            elif num == candidate_2:
+                counter_2 += 1
+
+        # Return only valid candidates
+        if counter_1 > threshold:
+            result.append(candidate_1)
+        if counter_2 > threshold:
+            result.append(candidate_2)
+
+        return result
